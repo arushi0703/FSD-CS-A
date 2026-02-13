d4e9fac9e99411f1b571e3be2aaf97430c5b2249
arushi0703 <contact2arushi@gmail.com>
2026-02-13 13:11:00 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,41 +1,55 @@
 import { on } from 'events';
 import http from 'http';
-import os from 'os';
-const port=4001;
-const server = http.createServer((req, res) =>{
-    const url=req.url;
-    if(url==='/' && req.method==='GET'){
-        res.write("<h1>Home Page</h1>");
+import os, { freemem } from 'os';
+const port = 4001;
+let body = "";
+const data = [];
+const server = http.createServer((req, res) => {
+
+    const url = req.url;
+    if (url === '/' && req.method === 'GET') {
+        res.end("<h1>Home Page</h1>");
     }
-    else if(url==='/about' && req.method==='GET'){
-        res.write("<h1>About Page</h1>");
+    else if (url === '/about' && req.method === 'GET') {
+        res.end("<h1>About Page</h1>");
     }
-    else if(url==='/contact' && req.method==='GET'){
-        res.write("<h1>Contact Page</h1>");
+    else if (url === '/contact' && req.method === 'GET') {
+        res.end("<h1>Contact Page</h1>");
     }
-    else if(url==='/system' && req.method==='GET'){
-        res.write("<h1>System Information</h1>");
+    else if (url === '/system' && req.method === 'GET') {
+        const sysdata = {
+            platform: os.platform(),
+            arch: os.arch(),
+            cpu: os.cpus().length,
+            Totalmem: os.totalmem()/1024**3 + " GB",
+            freemem: os.freemem()/1024**3 + " GB",
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(sysdata));
     }
-    else if (url==='/senddata' && req.method==='POST'){
-        const body="";
-        req.on('data',(chunk)=>{
-            body=body+chunk;
+    else if (url === '/senddata' && req.method === 'POST') {
+
+        req.on('data', (chunk) => {
+            body = body + chunk;
         })
-        req.on('end',()=>{
-        res.statusCode=201;
-        res.write(body + "<h1>View Data</h1>")
-        });
+        req.on('end', () => {
+            res.statusCode = 201;
+            console.log(body, "data send");
+            data.push(body);
+            res.end(JSON.stringify(data));
+        })
     }
-    else if (url==='/viewdata' && req.method==='GET'){
-        res.write("<h1>View Data</h1>");
+    else if (url === "/viewdata" && req.method === "GET") {
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(data));
     }
-    else{
-        res.statusCode=404;
-        res.write("<h1>Page Not Found</h1>");
+    else {
+        res.statusCode = 404;
+        res.end("<h1>Page Not Found</h1>");
     }
-    res.end();
+    // res.end();
 })
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
